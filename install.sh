37f1743b942a3848afef2ff9c9f2a9050d629d8b
Patrick Lingga <42988420+patrick8link@users.noreply.github.com>
2023-05-09 12:38:06 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install python3-dev libmysqlclient-dev
+sudo apt-get install python3-dev libmysqlclient-dev python3-pip
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 python3 -m pip instal pyopenssl
 python3 -m pip install pymongo regex flask flask_cors dict2xml mysqlclient zss pyangbind ncclient xmltodict pulp
