b773fd5c43ff9ce08ce9907e0f0b80e4113b99a8
Patrick Lingga <42988420+patrick8link@users.noreply.github.com>
2023-05-09 14:43:59 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo python3 get-pip.py
 
 curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 
-python3 -m pip install pymongo regex flask flask_cors dict2xml mysqlclient zss >
+python3 -m pip install pymongo regex flask flask_cors dict2xml mysqlclient zss pyangbind ncclient xmltodict pulp
 pip install pyopenssl
 
 source $HOME/.bashrc
