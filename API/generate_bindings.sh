c46d24a338201cb3ed8a31175ad5fd135caee420
Patrick Lingga <42988420+patrick8link@users.noreply.github.com>
2023-06-20 15:12:07 +0900

    Update generate_bindings.sh

--- API/generate_bindings.sh
+++ API/generate_bindings.sh
@@ -7,7 +7,7 @@ mkdir ~/yang/modules
 cp DataModel/ietf-i2nsf-capability.yang ~/yang/modules/.
 cp DataModel/ietf-inet-types.yang ~/yang/modules/.
 
-PYBINDPLUGIN=`/usr/bin/env python3 -c 'import pyangbind; import os; print ("%s/plugin" % os.path.dirname(pyangbind.__file__))'`
+PYBINDPLUGIN=`/usr/bin/env python3 -c 'import pybind.pyangbind as pyangbind; import os; print ("%s/plugin" % os.path.dirname(pyangbind.__file__))'`
 pyang --plugindir $PYBINDPLUGIN -f pybind --build-rpcs --split-class-dir $SDIR/rbindings DataModel/ietf-i2nsf-registration-interface.yang
 
 echo "Bindings successfully generated!"
