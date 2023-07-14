138f5ab1c6125ebd6b610b316f4ed13fac36641a
Patrick Lingga <42988420+patrick8link@users.noreply.github.com>
2023-07-14 18:10:56 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 source <(grep = controller.ini | sed 's/ *= */=/g')
 sed -i "s/172.24.4.120/${Controller}/" ~/i2nsf-security-controller/react/src/pages_components/*.js
 sed -i "s/172.24.4.120/${Controller}/" ~/i2nsf-security-controller/react/src/modals_components/*.js
+sed -i "s/10.0.0.44/${PrivateController}/" ~/i2nsf-security-controller/analytics/confd.conf
